477909f9d8a8d331187d97b50ca2f9bc57f63700
Ravi Shekhar <ravishekharrkm@gmail.com>
2025-08-08 14:27:12 +0530

    Initial commit

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+import Image from "next/image";
+import Navbar from "@/components/Navbar";
+import MenFashion from "@/components/MenFashion";
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows">
+      <Navbar />
+
+    </div>
+  );
+}
